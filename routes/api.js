ddcedb1b8cb492ce14cba1624393e8877fef37e0
Matthew Duffield <duffieldml@gmail.com>
2021-04-07 19:01:50 -0400

    Updating api routes to return a workout with a total duration

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,31 @@ const db = require("../models");
 //get workouts routes
 module.exports = (app) => {
   // All Workouts - will render last workout on page
-  // Creates Route
+  // Route for posting a new workout
   app.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body).then((dbWorkout) => {
       console.log(dbWorkout);
       res.json(dbWorkout);
     });
-  })
+  });
+
+  //Route for returning a workout
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ]).then((dbWorkout) => {
+      res.json(dbWorkout);
+      db.Workout.find({})
+        .then((dbWorkout) => {
+          res.json(dbWorkout);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    });
+  });
 }
